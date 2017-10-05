59edd0c27114483b8286439babd91e1020fb8db1
Josh Mejia <joshua.mejia@gmail.com>
2017-10-05 14:25:02 -0600

    Add code from class.

--- classroom.rb
+++ classroom.rb
@@ -1,15 +1,34 @@
 require 'pry'
 
 class Classroom
-  attr_reader :length, :width, :height
+  attr_reader :length, :width, :height, :students
 
-  def initialize(length, width, height)
+  def initialize(length, width, height = 10)
     @length = length
     @width  = width
     @height = height
+    @students = []
   end
 
   def area
     length * width
+  end
+
+  def volume
+    area * height
+  end
+
+  def add_length(feet)
+    @length += feet
+  end
+
+  def add_student(student)
+    @students << student
+  end
+
+  def print_roster
+    @students.each do |student|
+      puts student.name
+    end
   end
 end
--- classroom_runner.rb
+++ classroom_runner.rb
@@ -1,6 +1,16 @@
 require './classroom'
+require './student'
 require 'pry'
 
-classroom_a = Classroom.new(10, 5, 20)
-puts classroom_a.area
-puts classroom_a.length
+classroom_a = Classroom.new(10, 5)
+clarke_hall = Classroom.new(10, 5, 20)
+
+josh = Student.new("Josh")
+classroom_a.add_student(josh)
+
+classroom_a.print_roster
+
+ali = Student.new("Ali")
+classroom_a.add_student(ali)
+
+classroom_a.print_roster
